6822952aac86f3d8e4cba58c45fb3251a8f0f897
chrisologougiorgos <chrisologougiorgos@gmail.com>
2026-03-17 13:11:48 +0200

    Initial commit of SparK code

---
+++ models/custom.py
@@ -0,0 +1,123 @@
+# Copyright (c) ByteDance, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch
+import torch.nn as nn
+from typing import List
+from timm.models.registry import register_model
+
+import timm
+import torch.nn.functional as F
+
+
+class YourConvNet(nn.Module):
+    """
+    This is a template for your custom ConvNet.
+    It is required to implement the following three functions: `get_downsample_ratio`, `get_feature_map_channels`, `forward`.
+    You can refer to the implementations in `pretrain\models\resnet.py` for an example.
+    """
+    
+    def get_downsample_ratio(self) -> int:
+        """
+        This func would ONLY be used in `SparseEncoder's __init__` (see `pretrain/encoder.py`).
+        
+        :return: the TOTAL downsample ratio of the ConvNet.
+        E.g., for a ResNet-50, this should return 32.
+        """
+        return 32
+    
+    def get_feature_map_channels(self) -> List[int]:
+        """
+        This func would ONLY be used in `SparseEncoder's __init__` (see `pretrain/encoder.py`).
+        
+        :return: a list of the number of channels of each feature map.
+        E.g., for a ResNet-50, this should return [256, 512, 1024, 2048].
+        """
+        return [256, 512, 1024, 2048]
+    
+    def forward(self, inp_bchw: torch.Tensor, hierarchical=False):
+        """
+        The forward with `hierarchical=True` would ONLY be used in `SparseEncoder.forward` (see `pretrain/encoder.py`).
+        
+        :param inp_bchw: input image tensor, shape: (batch_size, channels, height, width).
+        :param hierarchical: return the logits (not hierarchical), or the feature maps (hierarchical).
+        :return:
+            - hierarchical == False: return the logits of the classification task, shape: (batch_size, num_classes).
+            - hierarchical == True: return a list of all feature maps, which should have the same length as the return value of `get_feature_map_channels`.
+              E.g., for a ResNet-50, it should return a list [1st_feat_map, 2nd_feat_map, 3rd_feat_map, 4th_feat_map].
+                    for an input size of 224, the shapes are [(B, 256, 56, 56), (B, 512, 28, 28), (B, 1024, 14, 14), (B, 2048, 7, 7)]
+        """
+        x = inp_bchw
+
+        x = self.backbone.conv1(x)
+        x = self.backbone.bn1(x)
+        x = self.backbone.act1(x)
+        x = self.backbone.maxpool(x)
+        
+        if hierarchical:
+            ls = []
+            x = self.backbone.layer1(x); ls.append(x)
+            x = self.backbone.layer2(x); ls.append(x)
+            x = self.backbone.layer3(x); ls.append(x)
+            x = self.backbone.layer4(x); ls.append(x)
+            return ls
+        else:
+            x = self.backbone.layer1(x)
+            x = self.backbone.layer2(x)
+            x = self.backbone.layer3(x)
+            x = self.backbone.layer4(x)
+
+            x = self.backbone.global_pool(x)
+            if self.backbone.drop_rate:
+                x = F.dropout(x, p=float(self.backbone.drop_rate), training=self.training)
+            x = self.backbone.fc(x)
+            return x
+
+
+
+@register_model
+def your_convnet_small(pretrained=False, **kwargs):
+    model = YourConvNet()
+    model.backbone = timm.create_model('resnext50_32x4d', pretrained=pretrained, **kwargs)
+
+    # ΠΑΓΩΜΑ
+    for param in model.backbone.parameters():
+        param.requires_grad = False
+     
+    return model
+
+
+@torch.no_grad()
+def convnet_test():
+    from timm.models import create_model
+    cnn = create_model('your_convnet_small')
+    print('get_downsample_ratio:', cnn.get_downsample_ratio())
+    print('get_feature_map_channels:', cnn.get_feature_map_channels())
+    
+    downsample_ratio = cnn.get_downsample_ratio()
+    feature_map_channels = cnn.get_feature_map_channels()
+    
+    # check the forward function
+    B, C, H, W = 4, 3, 224, 224
+    inp = torch.rand(B, C, H, W)
+    feats = cnn(inp, hierarchical=True)
+    assert isinstance(feats, list)
+    assert len(feats) == len(feature_map_channels)
+    print([tuple(t.shape) for t in feats])
+    
+    # check the downsample ratio
+    feats = cnn(inp, hierarchical=True)
+    assert feats[-1].shape[-2] == H // downsample_ratio
+    assert feats[-1].shape[-1] == W // downsample_ratio
+    
+    # check the channel number
+    for feat, ch in zip(feats, feature_map_channels):
+        assert feat.ndim == 4
+        assert feat.shape[1] == ch
+
+
+if __name__ == '__main__':
+    convnet_test()
---
+++ utils/imagenet.py
@@ -0,0 +1,122 @@
+# Copyright (c) ByteDance, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+from typing import Any, Callable, Optional, Tuple
+
+import PIL.Image as PImage
+from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+from torchvision.datasets.folder import DatasetFolder, IMG_EXTENSIONS
+from torchvision.transforms import transforms
+from torch.utils.data import Dataset
+
+try:
+    from torchvision.transforms import InterpolationMode
+    interpolation = InterpolationMode.BICUBIC
+except:
+    import PIL
+    interpolation = PIL.Image.BICUBIC
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f: img: PImage.Image = PImage.open(f).convert('RGB')
+    return img
+
+
+class ImageNetDataset(DatasetFolder):
+    def __init__(
+            self,
+            imagenet_folder: str,
+            train: bool,
+            transform: Callable,
+            is_valid_file: Optional[Callable[[str], bool]] = None,
+    ):
+        imagenet_folder = os.path.join(imagenet_folder, 'train' if train else 'val')
+        super(ImageNetDataset, self).__init__(
+            imagenet_folder,
+            loader=pil_loader,
+            extensions=IMG_EXTENSIONS if is_valid_file is None else None,
+            transform=transform,
+            target_transform=None, is_valid_file=is_valid_file
+        )
+        
+        self.samples = tuple(img for (img, label) in self.samples)
+        self.targets = None # this is self-supervised learning so we don't need labels
+    
+    def __getitem__(self, index: int) -> Any:
+        img_file_path = self.samples[index]
+        return self.transform(self.loader(img_file_path))
+
+
+
+#  Custom Κλάση για το ISIC DATASET
+class ISICDataset(Dataset):
+    def __init__(self, imagenet_folder, train, transform):
+    
+        self.folder = os.path.join(imagenet_folder, 'train' if train else 'val')
+        self.transform = transform
+        
+        
+        self.samples = [
+            os.path.join(self.folder, f) 
+            for f in os.listdir(self.folder) 
+            if f.lower().endswith(('.png', '.jpg', '.jpeg'))
+        ]
+        
+        if len(self.samples) == 0:
+            raise RuntimeError(f"Found 0 images in {self.folder}. Check your paths!")
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, index):
+        img_path = self.samples[index]
+        
+        img = pil_loader(img_path)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img 
+
+
+
+def build_dataset_to_pretrain(dataset_path, input_size) -> Dataset:
+    """
+    You may need to modify this function to return your own dataset.
+    Define a new class, a subclass of `Dataset`, to replace our ImageNetDataset.
+    Use dataset_path to build your image file path list.
+    Use input_size to create the transformation function for your images, can refer to the `trans_train` blow. 
+    
+    :param dataset_path: the folder of dataset
+    :param input_size: the input size (image resolution)
+    :return: the dataset used for pretraining
+    """
+
+    # Ίδια train transforms με το supervised pretraining
+    trans_train = transforms.Compose([
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomVerticalFlip(p=0.5),
+        transforms.RandomRotation(degrees=180),
+        transforms.ColorJitter(brightness=0.10, contrast=0.10, saturation=0.05, hue=0.00),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    
+    dataset_path = os.path.abspath(dataset_path)
+    for postfix in ('train', 'val'):
+        if dataset_path.endswith(postfix):
+            dataset_path = dataset_path[:-len(postfix)]
+    
+    dataset_train = ISICDataset(imagenet_folder=dataset_path, transform=trans_train, train=True)
+    print_transform(trans_train, '[pre-train]')
+    return dataset_train
+
+
+def print_transform(transform, s):
+    print(f'Transform {s} = ')
+    for t in transform.transforms:
+        print(t)
+    print('---------------------------\n')
